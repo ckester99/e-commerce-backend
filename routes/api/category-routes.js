fcca5b918c19e4d0786ad6084d924a052bbde128
Christian Kester <christiankester77@gmail.com>
2023-01-19 14:09:48 -0500

    changed category routes to accept information through the request body instead of route params

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -20,25 +20,32 @@ router.get("/:id", async (req, res) => {
     }
 });
 
-router.post("/:name", async (req, res) => {
+router.post("/", async (req, res) => {
+    /* req.body should look like this...
+    {
+      category_name: "Tools",
+    }
+  */
     try {
-        await Category.create({ category_name: req.params.name });
-        res.status(200).send(`${req.params.name} category created successfully!`);
+        await Category.create(req.body);
+        res.status(200).send(`${req.body.category_name} category created successfully!`);
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-router.put("/:id/:name", async (req, res) => {
+router.put("/:id/", async (req, res) => {
+    /* req.body should look like this...
+    {
+      category_name: "Tools",
+    }
+  */
     try {
         const oldName = await Category.findByPk(req.params.id).get("category_name");
-        await Category.update(
-            { category_name: req.params.name },
-            {
-                where: { id: req.params.id },
-            }
-        );
-        res.status(200).send(`${oldName} updated to ${req.params.name} successfully!`);
+        await Category.update(req.body, {
+            where: { id: req.params.id },
+        });
+        res.status(200).send(`${oldName} updated to ${req.body.category_name} successfully!`);
     } catch (err) {
         res.status(500).json(err);
     }
